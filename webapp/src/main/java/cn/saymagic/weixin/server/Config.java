8b7e96108811957d4c84ab96d820d067f3035047
sunshine <lzhy719@163.com>
2016-07-07 12:29:35 -0500

    token

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,7 +3,7 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "licy13@lenovo.com";
+	public static final String TOKEN = "licy13lenovo";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
 	public static final String APIKEY = "7be1091552220d9b20e7ed21cf79a3ab"; 
