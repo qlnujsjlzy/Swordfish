b0504ef9f3d73337d0e45efe350e7234d349547d
sunshine <lzhy719@163.com>
2016-07-07 11:46:42 -0500

    update Config
    token  and apikey

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "licy13@lenovo.com";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "7be1091552220d9b20e7ed21cf79a3ab"; 
 	
 }
